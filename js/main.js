b5609986a07c6cbb03c32de4c8f93f902a10201d
ErikAbrahamson <e.a.abrahamson@gmail.com>
2015-08-02 11:55:47 -0600

    Getting closer to solving 4

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ for (var i = 0; i < students.length - 1; i++) {
   console.log(students[i]);
 }
 //2
-for (var i = 0; i < students.length; i+=2) {
+for (var i = 0; i < students.length; i +=2) {
   console.log(students[i]);
 }
 //3
@@ -12,19 +12,10 @@ for (var i = students.length; i >= 0 ; i--) {
   console.log(students[i]);
 }
 //4
-// for (var i = 0; i < students.length; i++) {
-//   var studentsCopy = students.slice();
-//   var clone = null;
-//   for (var s = studentsCopy.indexOf('carl'); s <= 3 ; s++) {
-//     var clone = studentsCopy.splice(s, s + 1);
-//   }
-//   studentsCopy.push(clone);
-// }
-
-for (var i = 0; i < students.length; i++) {
+for (var i = 0; i <= students.length; i++) {
   var studentsCopy = students.slice();
   var clonedStudent = studentsCopy.slice(1,4);
-  for (var s = 1; s <= 3; s++) {
-    studentsCopy.splice(s, 0, clonedStudent[s]);
+  for (var c = 0; c < clonedStudent.length; c++) {
+    studentsCopy.splice(i + 2, 0, clonedStudent[c]);
   }
 }
